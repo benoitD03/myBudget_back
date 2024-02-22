4174e7bc13f0cb1d5306f662667f57d0cb5c205c
benoitD03 <bens.92@hotmail.fr>
2024-02-22 16:58:59 +0100

    Add favoris create & get methods

--- src/favoris/favoris.controller.ts
+++ src/favoris/favoris.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Query, UseGuards } from "@nestjs/common";
 import { FavorisService } from './favoris.service';
+import { AuthGuard } from "../auth/aut.guard";
+import { Categorie } from "../Categories/categorie.model";
+import { Favoris } from "./favoris.model";
+import { Sous_Categorie } from "../Sous-Categories/sous-categorie.model";
 
 @Controller('favoris')
 export class FavorisController {
   constructor(private readonly FavorisService: FavorisService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard)
+  @Get('all')
+  findAllByUserId(@Query() query): Promise<Favoris[]> {
+    return this.FavorisService.findAllByUserId(query);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard)
+  @Post()
+  createFavori(@Body() favoriData: Favoris): Promise<any> {
+    return this.FavorisService.createFavori(favoriData);
+  }
 }
--- src/favoris/favoris.service.ts
+++ src/favoris/favoris.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Between, FindManyOptions, Repository } from "typeorm";
 import { Favoris } from './favoris.model';
+import { Sous_Categorie } from "../Sous-Categories/sous-categorie.model";
+import { Categorie } from "../Categories/categorie.model";
 
 @Injectable()
 export class FavorisService {
   constructor(
     @InjectRepository(Favoris)
-    public Sous_Categories: Repository<Favoris>,
+    public Favoris: Repository<Favoris>,
   ) {}
+
+  /**
+   * Méthode de récupération des favoris d'un utlisateur
+   */
+  async findAllByUserId(userId: number): Promise<Favoris[] | undefined> {
+    const options: FindManyOptions = {
+      where: { user: userId },
+    };
+    const result = await this.Favoris.find(options);
+    return result;
+  }
+
+  /**
+   * Méthode de création d'un favori
+   */
+  async createFavori(FavoriData: Favoris): Promise<any> {
+    const favori = this.Favoris.create(FavoriData);
+    return this.Favoris.save(favori);
+  }
 }
